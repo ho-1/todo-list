5b7f5b28586d5198f484ab66a9e10d083516b87b
ByungHo <qudgh4290@gmail.com>
2022-01-14 23:44:31 +0900

    2-29 findByUserId 메서드 까지

---
+++ src/main/java/com/example/demo/persistence/TodoRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.persistence;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.TodoEntity;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity, String> {
+	// JpaRepository<T, ID> - T는 매핑될 엔티티 클래스, ID는 기본 키의 타입
+	List<TodoEntity> findByUserId(String userId);
+	
+	// ?1은 메서드의 매개변수의 순서 위치다.
+}
