fd3a14dd337b860313bcfb1ba70f60fe12a9cfbe
ByungHo <qudgh4290@gmail.com>
2022-01-23 16:50:47 +0900

    react router dom 추가 / 403 이면 로그인 페이지 리디렉트

---
+++ src/Login.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>로그인 페이지</p>
+      </div>
+    );
+  }
+}
+
+export default Login;
